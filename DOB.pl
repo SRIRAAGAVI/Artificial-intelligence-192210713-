7f3e56f0d3e2e26d22575137b0031316ea9b7e70
SRIRAAGAVI <132080032+SRIRAAGAVI@users.noreply.github.com>
2024-03-17 21:41:51 -0700

    Add files via upload

---
+++ DOB.pl
@@ -0,0 +1,8 @@
+% Database of names and dates of birth
+dob(john, '2000-05-15').
+dob(sarah, '1995-10-20').
+dob(michael, '1988-03-12').
+dob(emily, '1992-08-30').
+
+% Query to retrieve the date of birth given a name
+% Example usage: ?- dob(john, DOB).
